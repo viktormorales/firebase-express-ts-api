627a34037d13e972326f9d91e78598c030bfb519
Viktor H. Morales <viktorhugomorales@gmail.com>
2022-09-06 01:04:30 -0300

    update credential file

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -2,7 +2,9 @@ import * as functions from "firebase-functions";
 import * as admin from "firebase-admin";
 // Initialize firebase admin app
 admin.initializeApp({
-  credential: admin.credential.cert("./lib/serviceAccountKey.json"),
+  credential: admin.credential.cert(
+      "./lib/express-typescript-api-firebase-adminsdk-hc9td-2b5097761b.json"
+  ),
 });
 import * as express from "express";
 
