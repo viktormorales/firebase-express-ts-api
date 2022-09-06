8545f238eb39ccfce54b8a441981521d35fb483e
Viktor H. Morales <viktorhugomorales@gmail.com>
2022-09-06 13:56:19 -0300

    add dotenv

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,14 +1,20 @@
+// Import and configure dotenv
+import * as dotenv from "dotenv";
+dotenv.config({path: "../.env"});
+
+// Import and initialize firebase functions and admin
 import * as functions from "firebase-functions";
 import * as admin from "firebase-admin";
-// Initialize firebase admin app
 admin.initializeApp({
   credential: admin.credential.cert(
-      "./lib/express-typescript-api-firebase-adminsdk-hc9td-2b5097761b.json"
+      `${process.env.FIREBASE_CREDENTIAL_FILE}`
   ),
 });
+
+// Import Express server
 import * as express from "express";
 
-// Import POSTS routes
+// Import routes
 import postsRouter from "./routes/v1/posts";
 
 const app = express();
